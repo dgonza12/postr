ab8050a786a42e82bdf42484710a50bcc86b72a7
Danny Gonzalez <gdanny120@gmail.com>
2018-02-13 10:49:29 -0800

    adding all coponents to github.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,71 @@
 import { Component } from '@angular/core';
 
+// Firebase:
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
+import { Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/mergeMap';
+import 'rxjs/add/operator/map';
+import {ActivatedRoute} from '@angular/router';
+
+interface User {
+  id: string;
+  username: string;
+  about: string;
+  password: string;
+  image: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  isLoggedIn = false;
+  newPostMenu;
+  title: string;
+  content: string;
+  image: string;
+  username: string;
+  password: string;
+  userId: string;
+  users;
+  user;
+  constructor(private afs: AngularFirestore) {
+  }
+  LogIn() {
+    console.log('Trying to log in: ' + this.username + ' ' + this.password);
+    this.users = this.afs.collection('users', (ref) => ref.where('username', '==', this.username).limit(1)).valueChanges();
+    if ( this.users) {
+      console.log('User with username:' + this.username + ' exists');
+      this.users.subscribe((user) => {
+       this.user = (user[0]);
+       if (this.password === this.user.password) {
+         this.isLoggedIn = true;
+         this.userId = this.user.id;
+       } else {
+         alert('Password incorrect!');
+       }
+      });
+    }
+  }
+  LogOut() {
+    this.newPostMenu = false;
+    this.title = '';
+    this.content = '';
+    this.image = '';
+    this.isLoggedIn = false;
+    this.username = '';
+    this.password = '';
+  }
+  addPost() {
+    this.afs.collection('posts').add({'title': this.title, 'content': this.content, 'image': this.image, 'user': this.username});
+    this.togglePostMenu();
+  }
+  togglePostMenu() {
+    this.newPostMenu = !this.newPostMenu;
+    this.title = '';
+    this.content = '';
+    this.image = '';
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,63 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule} from '@angular/router';
 
 
+// Firebase
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyC4jeBSxA6EpX2Ktr9AgA0g25k9hlYXW4k',
+  authDomain: 'postr-840e6.firebaseapp.com',
+  databaseURL: 'https://postr-840e6.firebaseio.com',
+  projectId: 'postr-840e6',
+  storageBucket: 'postr-840e6.appspot.com',
+  messagingSenderId: '565782182875'
+};
+
+// Components
 import { AppComponent } from './app.component';
-
+import { PostsComponent } from './posts/posts.component';
+import { PostComponent } from './post/post.component';
+import { UserComponent } from './user/user.component';
+import { NewpostComponent } from './newpost/newpost.component';
+// Services
+import { PostServiceService} from './post-service.service';
+import { UserServiceService} from './user-service.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsComponent,
+    PostComponent,
+    UserComponent,
+    NewpostComponent
   ],
   imports: [
-    BrowserModule
+    FormsModule,
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule,
+    RouterModule.forRoot([
+      {
+        path: 'posts',
+        component: PostsComponent
+      },
+      {
+        path: 'post/:id',
+        component: PostComponent
+      },
+      {path: 'user/:id',
+       component: UserComponent
+      },
+      {path: 'newpost',
+       component: NewpostComponent
+      }
+    ])
   ],
-  providers: [],
+  providers: [PostServiceService, UserServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
